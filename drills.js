42191959b4bd8dea8a716d3a73d5f6fa293249c7
Melanie Hallowell <melleehall@gmail.com>
2020-05-16 22:12:55 -0600

    Added soln

--- drills.js
+++ drills.js
@@ -2,15 +2,12 @@
 // Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, 
 // identify the sequence of numbers that each recursive call will search to try and find 8.
 
+// 3
+
 // Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search 
 // algorithm, identify the sequence of numbers that each recursive call will search to try and find 16.
 
-
-
-
-
-
-
+// 5
 
 
 // 2. Adding a React UI
